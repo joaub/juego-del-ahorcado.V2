d371c095a972cc4431f1b0e374c9205e9e81647a
Joaquin Ugarte <ugartejoa12@gmail.com>
2025-11-20 15:19:15 -0300

    agregando mas palabras y categorias

--- src/palabras.js
+++ src/palabras.js
@@ -1,60 +1,105 @@
 export const palabras = {
-  animales: [
-    "perro", "gato", "elefante", "jirafa", "canguro",
-    "delfin", "ballena", "leon", "tigre", "ardilla","loro","aguila",
-    "serpiente","camaleon","hipopotamo","rinoceronte","zorro","mapache"
-  ],
+    animales: [
+        "perro", "gato", "elefante", "jirafa", "canguro",
+        "delfin", "ballena", "leon", "tigre", "ardilla", "loro", "aguila",
+        "serpiente", "camaleon", "hipopotamo", "rinoceronte", "zorro", "mapache",
+        "oso", "panda", "mono", "gorila", "cebra", "antilope",
+        "cocodrilo", "tortuga", "pez payaso", "estrella de mar",
+        "medusa", "pulpo", "calamar", "murcielago", "hormiga",
+        "abeja", "mariposa", "libelula", "araña", "escorpion"
+    ],
 
-  comidas: [
-    "pizza", "milanesa", "hamburguesa", "helado",
-    "ñoquis", "ravioles", "tortilla", "empanada","asado", "pastel de papa"
-  ],
+    comidas: [
+        "pizza", "milanesa", "hamburguesa", "helado", "chocolate",
+        "ensalada", "sopa", "paella", "ceviche", "sushi",
+        "lasagna", "fideos", "pollo", "pescado", "tacos",
+        "ñoquis", "ravioles", "tortilla", "empanada", "asado", "pastel de papa",
+        "churrasco", "arepas", "guiso", "budin", "crema catalana",
+        "flan", "pudin", "tarta", "croquetas", "gazpacho", "ratatouille"
+    ],
 
-  futbol: [
-    "river plate",
-    "boca juniors",
-    "san lorenzo",
-    "racing",
-    "independiente",
-    "rosario central",
-    "newells",
-    "estudiantes",
-    "gimnasia",
-    "velez",
-    "argentinos juniors",
-    "Gimnasia de mendoza",
-    "talleres de cordoba",
-    "colon de santa fe",
-    "independiente rivadavia",
+    futbol: [
+        "river plate", "boca juniors", "san lorenzo",
+        "racing", "independiente", "rosario central",
+        "newells", "estudiantes", "gimnasia", "velez",
+        "argentinos juniors", "Gimnasia de mendoza",
+        "talleres de cordoba", "colon de santa fe",
+        "independiente rivadavia", "godoy cruz",
+        "huracan", "lanus", "defensa y justicia",
 
-    "barcelona",
-    "real madrid",
-    "manchester united",
-    "manchester city",
-    "chelsea",
-    "juventus",
-    "inter",
-    "milan",
-    "paris saint germain",
-    "ajax","roma",
-    "napoli",
-    "bayern munich",
-    "liverpool",
-    "arsenal",
-    "atletico de madrid",
-    "tottenham"
-  ],
+        "barcelona", "real madrid", "manchester united",
+        "manchester city",
+        "chelsea", "juventus",
+        "inter", "milan", "whrexham",
+        "botafogo", "flamengo", "palmeiras",
+        "santos", "corinthians",
+        "paris saint germain",
+        "ajax", "roma", "chelsea",
+        "napoli", "bayern munich",
+        "liverpool", "arsenal",
+        "atletico de madrid", "tottenham",
+        "sevilla", "valencia",
+        "porto", "benfica"
+    ],
+    paises: [
+        "argentina", "brasil", "chile", "uruguay", "paraguay",
+        "bolivia", "peru", "colombia", "venezuela", "ecuador",
+        "mexico", "canada", "estados unidos", "cuba", "haiti",
+        "españa", "francia", "alemania", "italia", "portugal",
+        "inglaterra", "irlanda", "escocia", "gales", "paises bajos",
+        "belgica", "suiza", "austria", "polonia", "grecia",
+        "turquia", "rusia", "china", "japon", "corea del sur",
+        "india", "australia", "nueva zelanda", "sudafrica", "egipto",
+        "cabo verde", "marruecos", "argelia", "tunez", "libia","sudan",
+        "etiopia", "kenia", "uganda", "ghana", "nigeria"
+    ],
+    colores: [
+        "rojo", "azul", "verde", "amarillo", "naranja",
+        "morado", "rosa", "negro", "blanco", "gris",
+        "celeste", "violeta", "turquesa", "lila",
+        "beige", "cafe", "dorado", "plateado", "fucsia",
+        "magenta", "cyan", "lavanda", "ocre", "salmon",
+        "esmeralda", "zafiro", "rubí", "ámbar", "jade"
+    ],
+    objetos: [
+        "computadora", "telefono", "reloj", "bicicleta", "automovil",
+        "television", "camara", "libro", "lapicera", "mochila",
+        "escritorio", "silla", "mesa", "ventilador", "lampara",
+        "audifonos", "parlantes", "impresora", "teclado", "mouse",
+        "cargador", "maletin", "gafas", "sombrero", "chaqueta",
+        "zapatos", "botella", "taza", "plato", "cuchillo",
+        "tenedor", "cuchara", "almohada", "manta", "cama"
+    ],
 
-  dificil: [
-    "electrocardiograma",
-    "hipopotamo",
-    "jurisprudencia",
-    "murcielago",
-    "microorganismo",
-    "otorrinolaringologo",
-    "paralelepipedo",
-    "anticonstitucionalmente",
-    "desoxirribonucleico",
-    "esternocleidomastoideo"
-  ]
+    dificil: [
+        "electrocardiograma",
+        "hipopotamo",
+        "jurisprudencia",
+        "murcielago",
+        "microorganismo",
+        "otorrinolaringologo",
+        "paralelepipedo",
+        "anticonstitucionalmente",
+        "desoxirribonucleico",
+        "esternocleidomastoideo",
+        "inconstitucionalidad",
+        "neumoultramicroscopicosilicovolcanoconiosis",
+        "psiconeuroendocrinologia"
+    ],
+    raras: [
+        "murcielago",
+        "quinquagesimo",
+        "catarro",
+        "zigzag",
+        "marquesina",
+        "otorrinolaringologia",
+        "aclimatacion",
+        "clorofila",
+        "hiperpolarizado",
+        "transustanciacion",
+        "perpendicularidad",
+        "metamorfosis",
+        "hiperrealismo",
+        "cristalizacion"
+    ]
 };
